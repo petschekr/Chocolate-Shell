4e4ccb97fdebe99495a22daecd7dcfd8d5869e9e
Ryan Petschek <petschekr@gmail.com>
2016-05-28 20:37:15 -0400

    Implement working SSH client for fixed connection

--- app/js/chocolate.js
+++ app/js/chocolate.js
@@ -1,3 +1,29 @@
+var Client = require("ssh2").Client;
+var conn = new Client();
+
+let streamSSH;
+
+conn.on("ready", function () {
+	console.log("Client is ready");
+	conn.shell(function (err, stream) {
+		if (err) throw err;
+		streamSSH = stream;
+		stream.on("close", function () {
+			console.log("Stream closed");
+			conn.end();
+			streamSSH = null;
+		}).on("data", function (data) {
+			console.log("STDOUT: " + data);
+			// Convert from buffer to string values that term.js likes
+			term.write(data.toString());
+		}).stderr.on("data", function (data) {
+			console.log("STDERR: " + data);
+			term.write(data.toString());
+		});
+	});
+});
+
+
 var term = new Terminal({
 	cols: 100,
 	rows: 30	,
@@ -7,7 +33,13 @@ var term = new Terminal({
 });
 
 term.on("data", (data) => {
-	term.write(data);
+	//term.write(data);
+	if (streamSSH) {
+		streamSSH.write(data);
+	}
+	else {
+		console.warn("Connection not established!");
+	}
 });
 term.on("title", (title) => {
 	// Change title
@@ -15,3 +47,5 @@ term.on("title", (title) => {
 term.open(document.getElementsByClassName("content")[0]);
 
 term.write('\x1b[31mWelcome to term.js!\x1b[m\r\n');
+
+conn.connect({});
